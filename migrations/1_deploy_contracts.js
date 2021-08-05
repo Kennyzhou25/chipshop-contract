764589a732d6c429ffd84f0287cbcea6011959cf
codelirium <danny@cabinetex.com>
2021-08-06 01:08:13 +0900

    update: 1_deploy_contracts.js

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -142,8 +142,9 @@ async function afterMigration(deployer, network) {
   // console.log('fishRewardPool operation is finished.');
 
   const fishContract = await Fish.at(fishAddress);
-  const boardroomContract = await Boardroom.at(boardroomAddress);
+  const boardroomContract = await Boardroom.at(boardroomAddress)
   // await boardroomContract.initialize(chipAddress, fishAddress, treasuryAddress);
+  await chipContract.mint('0x0197aBAbE51C04FE2Be6D78331e4c9F20AE467DA', 100000);
   await fishContract.approve(boardroomAddress, MaxUint256);
   await boardroomContract.stake(10000);
   console.log('boardroom operation is finished.');
